912485a2d77d46b9357c16228a4c2af273620b41
Collins Wekesa <collinsdelan@gmail.com>
2018-11-23 12:24:48 +0300

    Update

--- index.js
+++ index.js
@@ -9,29 +9,30 @@ var connection = mysql.createConnection({
 });
 exports.handler = (event, context, callback) => {
     //var message = event.Records[0].Sns.Message;
-    console.log('Message received from SNS topic:', message);
+    // console.log('Message received from SNS topic:', message);
     connection.query('show slave status;', function (error, results, fields) {
         if (error) {
             connection.destroy();
             throw error;
         } else {
             var secondsBehind =results[0].Seconds_Behind_Master;
-            if (secondsBehind<10){
-                var sns = new AWS.SNS();
+            // if (secondsBehind<10){
+            //     var sns = new AWS.SNS();
 
-                sns.publish({
-                    Message: 'Test publish to SNS from Lambda',
-                    TopicArn: 'topicarn'
-                }, function(err, data) {
-                    if (err) {
-                        console.log(err.stack);
-                        return;
-                    }
-                    console.log('push sent');
-                    console.log(data);
-                    context.done(null, 'Function Finished!');
-                });
-            }
+            //     sns.publish({
+            //         Message: 'Test publish to SNS from Lambda',
+            //         TopicArn: 'topicarn'
+            //     }, function(err, data) {
+            //         if (err) {
+            //             console.log(err.stack);
+            //             return;
+            //         }
+            //         console.log('push sent');
+            //         console.log(data);
+            //         context.done(null, 'Function Finished!');
+            //     });
+            // }
+            console.log(secondsBehind, "uuuuu")
             callback(error, results);
             connection.end(function (err) { callback(err, results);});
         }
