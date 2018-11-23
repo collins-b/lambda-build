e8e933a7da4c92f1c0085273d0f94c6a4878a744
Collins Wekesa <collinsdelan@gmail.com>
2018-11-23 12:31:16 +0300

    Update

--- index.js
+++ index.js
@@ -1,63 +1,72 @@
-// UPDATED TODAY
-var mysql = require('mysql');
-var AWS = require('aws-sdk');
-var connection = mysql.createConnection({
-    host: process.env.HOST,
-    user: process.env.USER,
-    password: process.env.PASSWORD,
-    database: process.env.DATABASE,
-});
-exports.handler = (event, context, callback) => {
-    //var message = event.Records[0].Sns.Message;
-    // console.log('Message received from SNS topic:', message);
-    connection.query('show slave status;', function (error, results, fields) {
-        if (error) {
-            connection.destroy();
-            throw error;
-        } else {
-            var secondsBehind =results[0].Seconds_Behind_Master;
-            // if (secondsBehind<10){
-            //     var sns = new AWS.SNS();
-
-            //     sns.publish({
-            //         Message: 'Test publish to SNS from Lambda',
-            //         TopicArn: 'topicarn'
-            //     }, function(err, data) {
-            //         if (err) {
-            //             console.log(err.stack);
-            //             return;
-            //         }
-            //         console.log('push sent');
-            //         console.log(data);
-            //         context.done(null, 'Function Finished!');
-            //     });
-            // }
-            console.log(secondsBehind, "uuuuu")
-            callback(error, results);
-            connection.end(function (err) { callback(err, results);});
-        }
-    });
-};
-
-// console.log('Loading function');
-
+// // UPDATED
+// var mysql = require('mysql');
 // var AWS = require('aws-sdk');
-// AWS.config.region = 'us-east-1';
+// var connection = mysql.createConnection({
+//     host: process.env.HOST,
+//     user: process.env.USER,
+//     password: process.env.PASSWORD,
+//     database: process.env.DATABASE,
+// });
+// exports.handler = (event, context, callback) => {
+//     //var message = event.Records[0].Sns.Message;
+//     //console.log('Message received from SNS:', message);
+//     connection.query('show slave status;', function (error, results, fields) {
+//         if (error) {
+//             connection.destroy();
+//             throw error;
+//         } else {
+//             var secondsBehind =results[0].Seconds_Behind_Master;
+//             if (secondsBehind<10){
+//                 var sns = new AWS.SNS();
 
-// exports.handler = function(event, context) {
-//     console.log("\n\nLoading handler\n\n");
-//     var sns = new AWS.SNS();
-
-//     sns.publish({
-//         Message: 'Test publish to SNS from Lambda',
-//         TopicArn: 'arn:aws:sns:us-east-1:281094676178:job-now-slave'
-//     }, function(err, data) {
-//         if (err) {
-//             console.log(err.stack);
-//             return;
+//                 sns.publish({
+//                     Message: 'Test publish to SNS from Lambda',
+//                     TopicArn: 'arn:aws:sns:us-east-1:281094676178:job-now-slave'
+//                 }, function(err, data) {
+//                     if (err) {
+//                         console.log(err.stack);
+//                         return;
+//                     }
+//                     console.log('push sent');
+//                     console.log(data);
+//                     context.done(null, 'Function Finished!');
+//                 });
+//             }
+//             callback(error, results);
+//             connection.end(function (err) { callback(err, results);});
 //         }
-//         console.log('push sent');
-//         console.log(data);
-//         context.done(null, 'Function Finished!');
 //     });
 // };
+
+// // console.log('Loading function');
+
+// // var AWS = require('aws-sdk');
+// // AWS.config.region = 'us-east-1';
+
+// // exports.handler = function(event, context) {
+// //     console.log("\n\nLoading handler\n\n");
+// //     var sns = new AWS.SNS();
+
+// //     sns.publish({
+// //         Message: 'Test publish to SNS from Lambda',
+// //         TopicArn: 'arn:aws:sns:us-east-1:281094676178:job-now-slave'
+// //     }, function(err, data) {
+// //         if (err) {
+// //             console.log(err.stack);
+// //             return;
+// //         }
+// //         console.log('push sent');
+// //         console.log(data);
+// //         context.done(null, 'Function Finished!');
+// //     });
+// // };
+
+exports.handler = async (event) => {
+    // TODO implement
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify('Hello from Lambda!'),
+    };
+    return response;
+};
+
